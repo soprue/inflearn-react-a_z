807069e7dbd15eb89f7fa42d6ff1eb70425d00de
soprue <soprue@naver.com>
2022-10-30 00:51:48 +0900

    <feature> : <update Row Components> Row 컴포넌트 생성

--- react-netfilx-clone/src/components/Row.js
+++ react-netfilx-clone/src/components/Row.js
@@ -1,9 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import axios from "../api/axios";
+import "../css/Row.css";
 
-function Row() {
+function Row({ isLargeRow, title, id, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+   }
+
   return (
-    <div>Row</div>
+    <section className='row'>
+      <h2>{title}</h2>
+      <div className='slider'>
+        <div className='slider__arrow-left'>
+          <span className='arrow'
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >{"<"}</span>
+        </div>
+
+        <div id={id} className='row__posters'>
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              } `}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+
+        <div className='slider__arrow-right'>
+          <span className='arrow'
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >{">"}</span>
+        </div>
+      </div>
+    </section>
   )
 }
 
