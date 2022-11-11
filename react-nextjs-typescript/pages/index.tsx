a6d24b1dc5058e429525a7d6822ce7a05b59ad30
soprue <soprue@naver.com>
2022-11-12 00:21:47 +0900

    <feature> : <Apply pagination in next.js app> next.js에서 제공되는 파일 시스템 기반 라우터를 이용하여 라우팅 기능 구현 마크다운 파일을 html로 remark해서 컴포넌트에 프랍스로 내려 주기 (getStaticProps, getStaticPaths 활용)

---
+++ react-nextjs-typescript/pages/index.tsx
@@ -0,0 +1,56 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import { getSortedPostsData } from '../lib/post'
+import homeStyles from '../styles/Home.module.css'
+
+const Home = ({ allPostsData }: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) => {
+  return (
+    <div className={homeStyles.container}>
+      <Head>
+        <title>Gahee Kim</title>
+      </Head>
+
+      <section className={homeStyles.headingMd}>
+        <p>[Gahee Kim Introduction]</p>
+        <p>
+          (This is a website)
+        </p>
+      </section>
+
+      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+        <h2 className={homeStyles.headingLg}>Blog</h2>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({id, title, date}) => (
+            <li className={homeStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                {title}
+              </Link>
+              <br />
+              <small className={homeStyles.lightText}>
+                {date}
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
