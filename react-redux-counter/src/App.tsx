f68b43c7cf27d57e70eba12ba2eddae4fa44869b
soprue <soprue@naver.com>
2022-11-14 23:04:41 +0900

    <feature> : <Apply reducers> Provider combineReducers useSelector useDispatch

--- react-redux-counter/src/App.tsx
+++ react-redux-counter/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { RootState } from './reducers';
 import './App.css';
 
-function App() {
+type Props = {
+  value: any;
+  onIncrement: () => void;
+  onDecrement: () => void;
+}
+
+function App({ value, onIncrement, onDecrement }: Props) {
+  const dispatch = useDispatch();
+
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+
+  const [todoValue, setTodoValue] = useState("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  }
+
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", text: todoValue });
+    setTodoValue("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Clicked: {counter} times
+      <button onClick={onIncrement}>
+        +
+      </button>
+      <button onClick={onDecrement}>
+        -
+      </button>
+
+      <ul>
+        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
