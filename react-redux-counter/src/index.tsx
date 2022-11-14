7a0bc3a620addcccd8795ce6a94dc67c9cc39543
soprue <soprue@naver.com>
2022-11-14 23:11:36 +0900

    <feature> : <Apply loggerMiddleware> index.tsx에 로그 찍어내는 미들웨어 생성

--- react-redux-counter/src/index.tsx
+++ react-redux-counter/src/index.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer);
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store", store);
+  console.log("action", action);
+  next(action);
+}
+
+const middleware = applyMiddleware(loggerMiddleware);
+const store = createStore(rootReducer, middleware);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
